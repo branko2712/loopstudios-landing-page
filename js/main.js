297f2f5636d0214b1c054196bf3131fd5d835f21
branko2712 <branko2712@gmail.com>
2021-05-30 21:42:27 +0200

    fix small scroll issue on phone

--- js/main.js
+++ js/main.js
@@ -4,12 +4,18 @@ window.addEventListener('load', () => {
    if(mobileMenuButtonOpen){
        mobileMenuButtonOpen.addEventListener('click', ()=>{
         const mobileNavigation = document.querySelector('#mobileNavigation');
+        const htmlRootEl = document.querySelector('html');
         mobileNavigation.classList.add('active');
+        htmlRootEl.style.overflow = 'hidden';
 
         const closeButton = document.querySelector('#closeBtn');
 
         if(closeButton){
-            closeButton.addEventListener('click', () =>         mobileNavigation.classList.remove('active')
+            closeButton.addEventListener('click', () =>         {
+                mobileNavigation.classList.remove('active');
+                htmlRootEl.style.overflow = 'auto   ';
+
+            }
             );
         }
        });
